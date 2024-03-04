a7786373019fb597f97ebc58919d28ef48006399
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-04 19:27:28 +0100

    decode loop recipe

--- main.py
+++ main.py
@@ -112,6 +112,20 @@ def decode_arith(inst):
     return var + " = " + a + " " + operator + " " + b
 
 
+def decode_for_recipe(inst):
+    assert inst['op'] == 'for_recipe_ingredients'
+    assert '0' in inst
+    assert '1' in inst
+    assert '2' in inst
+
+    var = inst['1']
+    recipe = get_signal(inst['0'])
+
+    tgt = inst['2']
+
+    return "for " + var + " in recipe_ingredients(" + recipe + "): ", tgt
+
+
 def main():
     complex_sample_script = "DSC1Dh1Y0A1I1Bt2tR1NLvhY223I0u2Nomhi0ISJb52owTeG3JX3GP1hRRTO2gGSrG4J5Meb451rQ42vwdps1LljmK2XeBVa0jZytE4Ayl6B2C2cSK1Jz0582BbFd64SvrVO0bZMBJ2ilOVH1mSDfR2emb320CGaKz4PvDv13GnIsr3Jc6tC1FU4Px0nj01y24v1df0ral4M0dWVhV03jINe2T8hQf0aUrNC3nowiy45nVVS4d1Yqc08BeeK4WAW0d3CIfM72tn5Ym2tdv3h0zlPaJ2doZp63qa4uT2XWEmY1OJKEO28It8J2ke2AV03k3VN0gGAJY3pn4tI01gWee4Ojo5B41yXNs3JZ8Ts265QJ00Zwclq4Z0o4549ZUgN4ZcojH2IrjEk0ElTja2l3wZU02PAlW1xzkpV2mcvTu38oepn2SfHCj4crFIV27ilfc0O2Luc4Ixvyr1hIgXC1XvWbf3Jb0RK3GhNFR0NAp6j25gnAj1X1MKZ0YMGAY4XxYkU2IWvBH31izTV4AZK6f42lqXH4Ngf0N3yhzBs2wlvII3nUWcN09IFj62WWNfl4XR8RE3RKGlW0FgrBy3ei6wi0tR5aZ3yRAGE22Ff0h0y2FCQ45RP3B17HkpC2kyLFf4TsO0A4AoL2z3uQTPL4ejGqE34RNkA0ikIG73AchaR0S7zpD3qTrh41p5fLx0LGHBB2PIaxC2VVGRw0KWoJX4I9sLR22ee2R28xIX508zDwL4MPsoI0SrLnF2e38OX1KIcgj2hfrdY0DOJYy42DXVb0Y5Hpj3evNpM3dU8gT3GLJbu3eO8tV4GEDMl3sCiak0TYjVF3hOFJQ13P3qs16aAwv1k2n413Hbcq23681a43XXrU20cG0dp1X9skp2tpEgF1ISEvm1HdmZW2675aL0OH5aP4c0zeH0WpFJP3CVaxC3To1Fv0ZyCK61oR2xM09R8Gc25gh5Q4YT9co06BHrd1W2vXn2rBzuH3UsTZA396oAI23LlWd0cJzqw0J8eBi1nPrQP3LN0932hHkMg3E5F4k0b1MTJ0dM06y4WHezk2VaHOv3yWgUF0cqyIS16UOHK1Xd2mv4fjzKO0MXFSh"
 
@@ -119,23 +133,34 @@ def main():
     # as_dict = get_dict_from_desynced_str(complex_sample_script)
     as_dict = as_dict_complex
 
-    print(get_function_name(as_dict))
-
     pnames = as_dict['pnames']
     name = as_dict['name']
 
     code_str = get_function_name(as_dict) + "\n"
     nesting_lvl = 1
+    next_loop_end = []
+
     for key, instr in as_dict.items():
         if key.isdigit():
+            if len(next_loop_end) >0 and int(key) == next_loop_end[0]:
+                nesting_lvl -= 1
+                assert nesting_lvl > 0
+                next_loop_end = next_loop_end[1:]
+
             for i in range(nesting_lvl):
                 code_str += '\t'
             if instr['op'] == 'set_number':
                 code_str += decode_set_number(instr)
             elif instr['op'] in ('add', 'sub', 'mul', 'div'):
                 code_str += decode_arith(instr)
+            elif instr['op'] == 'for_recipe_ingredients':
+                nesting_lvl += 1  # begin loop
+                st, tgt = decode_for_recipe(instr)
+                next_loop_end.insert(0, tgt - 1)
+                code_str += st
             else:
-                code_str += "UNKNOWN_OPCODE"
+                code_str += "UNKNOWN_OPCODE: " + instr['op']
+            # print(instr)
 
             code_str += '\n'
 
