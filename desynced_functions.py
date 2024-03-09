ee3aeb0c2a503159df1282fc8c88f03f14044489
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-09 21:27:24 +0100

    added stubs for functions so that an ide can be used

---
+++ desynced_functions.py
@@ -0,0 +1,4 @@
+# stubs of functions for usage with an IDE
+def recipe_ingredients(a):
+    return a
+
--- sample_input.py
+++ sample_input.py
@@ -1,18 +1,14 @@
-# def Factory_Block_Manager(Good_Produced):
-#	A = [1, None]
-#	for B in recipe_ingredients(P1):
-#		A = A + 1
-#	B = count_slots(STORAGE)
-#	C = B / A
-#	D = [1, None]
+from desynced_functions import *
 
-"""Sample Function"""
+"""Sample Function Description """
 def sample(parameter_name):
+
     local_variable = [1, None]
     local_variable = parameter_name + local_variable
     if parameter_name <= local_variable:
         print("LEQ")
     else:
         print("GT")
-    print("END")
+    for ingr in recipe_ingredients(parameter_name):
+        print([ingr,"In Recipe"])
 
