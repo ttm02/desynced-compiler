2952d942922e9162759d3c82637a1d2c7b361dec
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-10 11:55:39 +0100

    moved file

--- examples/sample_input.py
+++ examples/sample_input.py
@@ -1,15 +1,15 @@
 from desynced_functions import *
 
 """Sample Function Description """
-def sample(parameter_name):
 
+
+def sample(parameter_name):
     local_variable = [1, None]
     local_variable = parameter_name + local_variable
     if parameter_name <= local_variable:
-        print("LEQ")
+        print("LESS OR EQ")
     else:
-        print("GT")
+        print("GREATER")
     for ingr in recipe_ingredients(parameter_name):
-        print([ingr,"In Recipe"])
-    print("END FOR")
-
+        print([ingr, "In Recipe"])
+    print("END OF PROGRAM")
