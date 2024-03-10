899dd8e1fe0baccefa82e37e87171a59e6a802a4
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-10 10:58:08 +0100

    add scripts that extract relevant information from instruction table and creates the desynced_functions.py function stubs

--- read_instruction_table.py
+++ read_instruction_table.py
@@ -4,28 +4,79 @@ import requests
 CSV_FILE_NAME = "desynced_instructions.csv"
 
 
-def update_instruction_table_from_url():
+def fetch_instruction_table_from_url():
     url = r'https://wiki.desyncedgame.com/Special:CargoTables/instruction'
-    #header = {
+    # header = {
     #    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
     #    "X-Requested-With": "XMLHttpRequest"
-    #}
-    #r = requests.get(url, headers=header)
+    # }
+    # r = requests.get(url, headers=header)
     r = requests.get(url)
     # directly reading with pandas from the url gives 403 forbidden
     dfs = pd.read_html(r.text)
 
     assert (len(dfs) == 1)
     df = dfs[0]
-    df.to_csv(CSV_FILE_NAME)
+    return df
 
 
 def main():
-    #update_instruction_table_from_url()
+    df = fetch_instruction_table_from_url()
 
-    df = pd.read_csv(CSV_FILE_NAME)
+    # containing only the information needed by the compile step
+    # name is the index
+    df_result = pd.DataFrame(columns=['num_args', 'output_arg_num', 'is_iterator'])
+    # print(df.columns)
 
-    print(df)
+    # generate function stubs
+    result_str = "# stubs of functions for usage with an IDE"
+
+    for index, row in df.iterrows():
+        name = row['luaId']
+        is_iterator = False
+        if name.startswith('for_'):
+            name = name[4:]
+            is_iterator = True
+        output_name = None
+        output_pos = pd.NA
+        args = []
+        cfg_edge_args = []
+        for i in range(1, 12):
+            arg_type = row['argsType' + str(i)]
+            arg_name = row['argsName' + str(i)]
+            arg_datatype = row['argsDataType' + str(i)]
+            if not pd.isna(arg_type):
+                assert arg_type in ["Input", "Output", "Exec"]
+                if arg_type == "Input" or (arg_type == "Output" and output_name is not None):
+                    arg_name = arg_name.lower().replace(' ', '_')  # into pythonic form
+                    args.append(arg_name)
+                if arg_type == "Output" and output_name is None:
+                    arg_name = arg_name.lower().replace(' ', '_')  # into pythonic form
+                    output_name = arg_name
+                    output_pos = i
+                if arg_type == "Exec":
+                    cfg_edge_args.append(arg_name)
+        result_str += "\n\ndef " + name + "("
+
+        for arg in args:
+            result_str += arg + ", "
+
+        if len(args) > 0:
+            result_str = result_str[:-2]  # remove trailing ,
+        result_str += "):\n"
+        if is_iterator:
+            result_str += "\tyield "
+        else:
+            result_str += "\treturn "
+        if output_name is not None:
+            result_str += "'" + output_name + "'\n"
+        else:
+            result_str += "None\n"
+        df_result.loc[name] = [len(args), output_pos, is_iterator]
+
+    with open("desynced_functions.py", "w") as f:
+        f.write(result_str)
+    df_result.to_csv(CSV_FILE_NAME,index_label="name")
 
 
 if __name__ == "__main__":
